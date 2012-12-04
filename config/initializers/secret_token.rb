2f58d7d35ada98a7f59cba8107d1ce49d612bfb8
Jeremy Vandersay <jeremyv@tpg.com.au>
2012-12-04 18:12:01 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResourcePlanner::Application.config.secret_token = '36e2024b08fd510a054654f26493b5aa779dfc4895c40b2233959c44167d1f48809607759b3d5ef4a78286963807d181455fad7b7a0443836c3ea8e002d617ab'
